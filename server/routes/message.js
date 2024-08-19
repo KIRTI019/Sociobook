a7e9a07f2e56234f53bcab44c5b519a7a20cf08e
Kirti Kumar <113838803+KIRTI019@users.noreply.github.com>
2024-08-19 13:54:12 +0530

    Update message.js

--- server/routes/message.js
+++ server/routes/message.js
@@ -1,36 +1,10 @@
-import Message from "../models/Message.js";
+import express from "express";
+import { getMessages, sendMessage } from "../controllers/message.js";
+import { verifyToken } from "../middleware/token.js";
 
-export const getMessages = async (req, res) => {
-  try {
-    const { senderId, receiverId } = req.body;
+const router = express.Router();
 
-    const messages = await Message.find({
-      $or: [
-        { sender: senderId, receiver: receiverId },
-        { sender: receiverId, receiver: senderId },
-      ],
-    }).sort({ createdAt: 1 });
+router.post("/", getMessages);
+router.post("/send-message", sendMessage);
 
-    res.status(200).json(messages);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-export const sendMessage = async (req, res) => {
-  try {
-    const { sender, receiver, content } = req.body;
-
-    const message = new Message({
-      sender,
-      receiver,
-      content,
-    });
-
-    const savedMessage = await message.save();
-
-    res.status(201).json(savedMessage);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
+export default router;
